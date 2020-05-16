4128ced094b8affe04fa660a294e7c9ebc89b5c8
Nathan Keller <keller.nathan@gmail.com>
2020-05-16 11:41:52 -0600

    adjust spacing on find_articles method

--- app/models/tagcollection.rb
+++ app/models/tagcollection.rb
@@ -7,7 +7,8 @@ class Tagcollection < ApplicationRecord
   acts_as_taggable_on :tags
 
   def find_articles
-    self.articles = Article.where("created_at >= ?", 1.week.ago).
+    self.articles = Article.
+      where("created_at >= ?", 1.week.ago).
       tagged_with(tag_list, any: true).
       order(page_views_count: :desc).
       limit(5)
