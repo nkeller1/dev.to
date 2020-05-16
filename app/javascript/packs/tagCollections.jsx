9c58b80fa2f28ab212bf67bfe5e8cbf632f3bd1f
monstaro <codysmith2814@gmail.com>
2020-05-16 16:12:54 -0600

    Collections display from dataset

--- app/javascript/packs/tagCollections.jsx
+++ app/javascript/packs/tagCollections.jsx
@@ -2,15 +2,24 @@ import { h, render } from 'preact';
 import { getUserDataAndCsrfToken } from '../chat/util';
 import { TagCollections } from '../tagCollections/tagCollections';
 
+// var tagCollections = <%= @tagcollection %>
+
 function loadElement() {
-  getUserDataAndCsrfToken().then(() => {
+  getUserDataAndCsrfToken().then(({ currentUser }) => {
+    console.log('aa', currentUser);
     const root = document.getElementById('tag-collections');
+    // console.log('b', root.dataset.collections)
+    const { collections } = root.dataset;
+    // const arr = collections.split("\")
+    const a = collections.split('"name":"');
+    const pre = a.map(item => item.toString().split('"'));
+    pre.shift();
+    const names = pre.map(item => item[0]);
+    // const tags = collections.split('tag_list')
+    // console.log(names, tags)
     if (root) {
       render(
-        <TagCollections
-          availableTags={['front end', 'back end']}
-          statusView={root.dataset.view}
-        />,
+        <TagCollections availableTags={names} statusView={root.dataset.view} />,
         root,
         root.firstElementChild,
       );
